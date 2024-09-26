e94587947c8c952507c6cf09edd22e1ba89d355d
a22iagopp <a22iagopp@iessanclemente.net>
2024-09-26 14:27:35 +0200

    Proyecto finalizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+/**
+ * Main class: receives a number input by keyboard and gives an
+ * ouput of the number of 1s it would have if converted to binary
+ * */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int edad = sc.nextInt();
+        int numvelas = 0;
+        String binario = Integer.toBinaryString(edad);
+
+        for (int i = 0; i < binario.length(); i++) {
+            if(binario.charAt(i)=='1'){
+                numvelas++;
+            }
         }
+        System.out.println(numvelas);
     }
 }
