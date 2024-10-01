3b813462b70c1a4a214418dc9edfd234cbc68695
a22iagopp <a22iagopp@iessanclemente.net>
2024-10-01 09:25:20 +0200

    Completado ejercicio: Review Exercise 2 - Java command line arguments - Birthday

---
+++ src/main/java/org/example/BirthdayArgs.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class BirthdayArgs {
+    public static void main(String[] args) {
+        if(args.length==0) {
+            System.out.println("Use: org.example.BirthdayArgs n num1 num2 ... numn");
+        }else{
+            int numCases = Integer.parseInt(args[0]);
+            if (numCases==args.length-1){
+                int cases = 1;
+                while (cases <=numCases){
+                    int age = Integer.parseInt(args[cases]);
+                    String binaryAge = Integer.toBinaryString(age);
+                    int cont = 0;
+                    for (int i = 0; i < binaryAge.length() ; i++) {
+                        if (binaryAge.charAt(i)=='1'){
+                            cont++;
+                        }
+                    }
+                    System.out.println(cont);
+                    cases++;
+                }
+            }else{
+                System.out.println("Use: BirthdayArgs n num1 num2... numn");
+            }
+        }
+    }
+}
